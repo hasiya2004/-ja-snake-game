815b22d8eb6812a9e0b8a420895cfd2bb1cf11a0
Hasindu <85918275+hasiya2004@users.noreply.github.com>
2022-09-02 22:18:47 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+let username;
+
+document.getElementById("myButton").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = username;
+
+}
